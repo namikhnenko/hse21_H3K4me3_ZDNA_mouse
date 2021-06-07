0ea7b94e2a8afedd0c2f4c87b68656a077894498
namikhnenko <57178126+namikhnenko@users.noreply.github.com>
2021-06-07 23:14:21 +0300

    libs

---
+++ src/lib.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+###
+
+DATA_DIR <- 'C:/Users/User/Desktop/Bioinformatics/H3K4me3_ES_E14/data/'
+OUT_DIR <- 'C:/Users/User/Desktop/Bioinformatics/H3K4me3_ES_E14/images'
