c394f1cdef2401889d1ea5f13294eb63151ea848
namikhnenko <57178126+namikhnenko@users.noreply.github.com>
2021-06-08 01:29:28 +0300

    Add files via upload

---
+++ src/chip_seeker.R
@@ -0,0 +1,34 @@
+setwd('C:/Users/User/Desktop/Bioinformatics/H3K4me3_ES_E14/')
+source('lib.R')
+
+###
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+   install.packages("BiocManager")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+BiocManager::install("ChIPseeker")
+BiocManager::install("clusterProfiler")
+BiocManager::install("ChIPpeakAnno")
+BiocManager::install("org.Mm.eg.db")
+
+ 
+library(ChIPseeker)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+library(org.Mm.eg.db)
+
+###
+
+#NAME <- 'DeepZ'
+#NAME <- 'H3K4me3_ES_E14.ENCFF993IIG.mm10.filtered'
+NAME <- 'H3K4me3_ES_E14.ENCFF899LDH.mm10.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Mm.eg.db")
+
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.annopie.pdf'))
+plotAnnoPie(peakAnno)
+dev.off()
